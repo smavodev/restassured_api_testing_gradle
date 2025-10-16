3e6637888659d92b5dd1d76d63d116990906d65b
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2025-10-15 20:32:27 -0500

    feat: add examples json path, json object

--- src/test/java/basicRestAssured/BasicRestAssured.java
+++ src/test/java/basicRestAssured/BasicRestAssured.java
@@ -1,9 +1,11 @@
 package basicRestAssured;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.equalTo;
 
 public class BasicRestAssured {
@@ -43,6 +45,42 @@ public class BasicRestAssured {
                 .statusCode(200)
                 .body("Content", equalTo("Project Rest"))
                 .body("Icon", equalTo(4));
+    }
+
+    @Test
+    public void createProjectJsonObject () {
+
+        JSONObject bodyRequest = new JSONObject();
+        bodyRequest.put("Content", "Project Rest Object");
+        bodyRequest.put("Icon", 4);
+
+        given().auth().preemptive().basic("martinvo151@gmail.com", "1nd1.sm4rT%%")
+                .header("Content-Type", "application/json")
+                .body(bodyRequest.toString())
+                .log().all()
+                .when().post("https://todo.ly/api/projects.json")
+                .then()
+                .log().ifValidationFails()
+                .statusCode(200)
+                .body("Content", equalTo("Project Rest Object"))
+                .body("Icon", equalTo(4));
+    }
+
+    @Test
+    public void createProjectJsonExternal () {
+
+        String createProjectJson = new File("").getAbsolutePath()+"/src/test/resources/createProject.json";
+
+        given().auth().preemptive().basic("martinvo151@gmail.com", "1nd1.sm4rT%%")
+                .header("Content-Type", "application/json")
+                .body(new File(createProjectJson))
+                .log().all()
+                .when().post("https://todo.ly/api/projects.json")
+                .then()
+                .log().ifValidationFails()
+                .statusCode(200)
+                .body("Content", equalTo("Project Json External"))
+                .body("Icon", equalTo(5));
     }
 
 }
